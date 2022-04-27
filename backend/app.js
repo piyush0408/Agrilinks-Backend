13d186fe5d3c387ba60e87e4e2dc1012c529e25b
Piyush Lohiya <lohiya.piyush04@gmail.com>
2022-04-28 00:50:31 +0530

    my Commit

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(fileUpload());
 
 // //route imports
 // const product= require("./routes/productRoute");
-const user = require("./routes/userRoute");
+const user = require("./routes/reportRoute");
 // const order = require("./routes/orderRoute");
 // app.use("/api/v1",product);
 app.use("/api/v1", user);
